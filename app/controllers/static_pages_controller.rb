24bfa7664a3849eed7d5b8edf804a2b018fd286b
Satyajeet Pal <psatyajeet@gmail.com>
2012-08-22 17:59:50 -0400

    Add list of restaurants in Pboro

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
 
   def home
 
-  #@client = GooglePlaces::Client.new("AIzaSyD0-q-S5dQK7ph84LrAoR3oTaf6rxmDdaw")
-  #@spotList = @client.spots(40.340395,-74.586894, :types => 'restaurant', :radius => "1000")
+  @client = GooglePlaces::Client.new("AIzaSyD0-q-S5dQK7ph84LrAoR3oTaf6rxmDdaw")
+  @spotList = @client.spots(40.340395,-74.586894, :types => 'restaurant', :radius => "1000")
   @search='hello'
     if signed_in?
       @micropost  = current_user.microposts.build
