b203e895eec06643610cce8af0125ba29296d392
Satyajeet Pal <psatyajeet@gmail.com>
2012-08-23 14:43:51 -0400

    Clean up radius and create locations model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,7 +13,7 @@ class StaticPagesController < ApplicationController
 
   def search
     @client = GooglePlaces::Client.new("AIzaSyD0-q-S5dQK7ph84LrAoR3oTaf6rxmDdaw")
-    @spotList = @client.spots(40.340395,-74.586894, :name => params[:keyword], :types => params[:type], :radius => '1000')
+    @spotList = @client.spots(40.340395,-74.586894, :name => params[:keyword], :types => params[:type], :radius => params[:dist].to_i*1000)
   end
 
   def 
