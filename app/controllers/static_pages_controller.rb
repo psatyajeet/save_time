56d4f55e42afd9d332d9915a677951f0b327614d
Satyajeet Pal <psatyajeet@gmail.com>
2012-08-23 23:46:15 -0400

    Add address and make app faster

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,6 @@ class StaticPagesController < ApplicationController
 
   def home
 
-  #@client = GooglePlaces::Client.new("AIzaSyD0-q-S5dQK7ph84LrAoR3oTaf6rxmDdaw")
-  #@spotList = @client.spots(40.340395,-74.586894, :types => 'restaurant', :radius => "1000")
     if signed_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
